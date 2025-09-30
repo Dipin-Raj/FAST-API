7088e1fde1d303eb16b97fbb245c0f94f231eae8
Dipin Raj <124481530+Dipin-Raj@users.noreply.github.com>
2025-09-30 16:10:39 +0530

    Update streamlit_app.py

--- Core/streamlit_app.py
+++ Core/streamlit_app.py
@@ -4,7 +4,7 @@ from datetime import date
 import os
 
 # FastAPI base URL - Read from environment variable for deployment, with a local fallback
-FASTAPI_URL = os.getenv("FASTAPI_URL", "http://localhost:8000")
+FASTAPI_URL = os.getenv("https://fast-api-o2y7.onrender.com/", "http://localhost:8000")
 
 st.title("🏥 HMS Patient Management")
 
