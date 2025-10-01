02b9c352b0dff9e898609221c2554201f4d065c7
Dipin Raj <124481530+Dipin-Raj@users.noreply.github.com>
2025-10-01 10:16:02 +0530

    Update models.py

--- app/models.py
+++ app/models.py
@@ -7,8 +7,9 @@ class Patient(Base):
 
     id = Column(Integer, primary_key=True, index=True)
     name = Column(String, nullable=False)
-    email = Column(String, unique=True, nullable=False, index=True)
+    middle_name = Column(String, nullable=True) # ADD THIS LINE
+    email = Column(String, unique=True, index=True, nullable=False)
     date_of_birth = Column(Date, nullable=False)
     phone_number = Column(String, nullable=True)
     address = Column(String, nullable=True)
-    created_at = Column(DateTime, server_default=func.now())
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
